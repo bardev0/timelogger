79cd28713859f9d65ecb5782d88530e8b73bfd2b
greg <hzststhz@outlook.com>
2023-07-21 22:27:42 +0200

    simple switching of command line args

--- src/cmd.ts
+++ src/cmd.ts
@@ -2,4 +2,22 @@ import process from "process";
 
 const args = process.argv.splice(2)
 
+//debug
 console.log(args)
+
+switch (args[0]) {
+	case "--start" :
+		console.log("start")
+		break;
+	case "--end" :
+		console.log("break")
+		break;
+	case "--list":
+		console.log("list")
+		break;
+	case "--total":
+		console.log("total")
+		break;
+	default:
+		console.log("Wrong agruments")
+}
